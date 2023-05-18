5417597d59de2ac0ceafe456ae55cfbd6e54fb50
naielv <109038805+naielv@users.noreply.github.com>
2023-05-18 16:33:40 +0200

    Update boot.py

--- pico/boot.py
+++ pico/boot.py
@@ -1,5 +1,25 @@
-## DELETE THIS FILE TO USE CUSTOM CODE ##
-import urequests
-r = urequests.request("GET", "https://github.com/naielv/AxelWebServer/releases/latest/download/files.json")
-with open("files.json", "w") as f:
-    f.write(r.text)
+import network
+import time
+from secrets import SSID, PASS
+
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+wlan.connect(SSID, PASS)  # ssid, password
+
+# connect the network
+wait = 30
+while wait > 0:
+    if wlan.status() < 0 or wlan.status() >= 3:
+        break
+    wait -= 1
+    print("waiting for connection...")
+    time.sleep(1)
+
+# Handle connection error
+if wlan.status() != 3:
+    raise RuntimeError("wifi connection failed")
+else:
+    print("connected")
+    ip = wlan.ifconfig()[0]
+    print("IP: ", ip)
+
